454bc0b5ad6ec3da8fc5e0275835e0646308f2ec
pdh903 <pdh90345@gmail.com>
2024-04-21 18:08:19 +0900

    linkedlist 풀이

--- study_30/bj_1406.py
+++ study_30/bj_1406.py
@@ -2,24 +2,75 @@
 # 다시 풀어보기
 import sys
 
-s1 = list(map(str, sys.stdin.readline().strip()))
-s2 = []
+# s1 = list(map(str, sys.stdin.readline().strip()))
+# s2 = []
+# n = int(input())
+
+# for _ in range(n):
+#     command = list(sys.stdin.readline().strip().split())
+#     if len(command) == 1:
+#         if command[0] == "L":
+#             if len(s1) > 0:
+#                 s2.append(s1.pop())
+#         elif command[0] == "D":
+#             if len(s2) > 0:
+#                 s1.append(s2.pop())
+#         elif command[0] == "B":
+#             if s1:
+#                 s1.pop()
+#     else:
+#         s1.append(command[1])
+
+# s1.extend(reversed(s2))
+# print("".join(s1))
+
+
+class ListNode:
+    def __init__(self, value, prev, next):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+
+head = ListNode(0, None, None)
+cur = head
+
+arr = list(sys.stdin.readline().strip())
 n = int(input())
 
+for i in range(len(arr)):
+    newNode = ListNode(arr[i], None, None)
+    cur.next = newNode
+    newNode.prev = cur
+    cur = newNode
+
 for _ in range(n):
-    command = list(sys.stdin.readline().strip().split())
+    command = list(sys.stdin.readline().split())
     if len(command) == 1:
         if command[0] == "L":
-            if len(s1) > 0:
-                s2.append(s1.pop())
+            if cur.value != 0:
+                cur = cur.prev
         elif command[0] == "D":
-            if len(s2) > 0:
-                s1.append(s2.pop())
+            if cur.next != None:
+                cur = cur.next
         elif command[0] == "B":
-            if s1:
-                s1.pop()
+            if cur.value != 0:
+                cur = cur.prev
+                if cur.next.next:
+                    cur.next = cur.next.next
+                    cur.next.prev = cur
+                else:
+                    cur.next = None
     else:
-        s1.append(command[1])
+        newNode = ListNode(command[1], None, None)
+        if cur.next:
+            newNode.next = cur.next
+            cur.next.prev = newNode
+        cur.next = newNode
+        newNode.prev = cur
+        cur = newNode
 
-s1.extend(reversed(s2))
-print("".join(s1))
+cur = head.next
+while cur != None:
+    print(cur.value, end="")
+    cur = cur.next
