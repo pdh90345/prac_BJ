7d8d71c9abec1065694f6f7e7d4c516a91a74968
pdh903 <pdh90345@gmail.com>
2024-07-11 23:04:21 +0900

    commit

---
+++ study_30/bj_2531.py
@@ -0,0 +1,45 @@
+# 회전 초밥
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+n, d, k, c = map(int, input().split())
+
+plates = set()
+d = deque()
+arr = []
+for i in range(n):
+    if i < k:
+        d.append(int(input().strip()))
+        plates.add(d[-1])
+        continue
+    arr.append(int(input().strip()))
+
+for item in d:
+    arr.append(int(item))
+
+if c in plates:
+    ans = len(plates)
+else:
+    ans = len(plates) + 1
+
+for i in range(n - 1):
+    out = d.popleft()
+    plates.remove(out)
+    if out in d:
+        plates.add(out)
+
+    d.append(arr[i])
+    plates.add(arr[i])
+
+    if c in plates:
+        temp = len(plates)
+    else:
+        temp = len(plates) + 1
+
+    ans = max(ans, temp)
+
+print(ans)
