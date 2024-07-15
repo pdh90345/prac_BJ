4652bb6659eaa2d910abe831ad067f3e2b0e84d6
pdh903 <pdh90345@gmail.com>
2024-07-15 09:46:11 +0900

    commit

--- 0x05 스택/bj_17298.py
+++ 0x05 스택/bj_17298.py
@@ -8,9 +8,10 @@ chk_arr = list(-1 for _ in range(n))
 
 stack = []
 for i, value in enumerate(arr):
+    # 스택안의 value가 현재 배열의 value 보다 작으면 현재 value가 오큰수 이다.
     while stack and stack[-1][1] < value:
-        index = stack.pop()[0]
-        chk_arr[index] = value
+        index = stack.pop()[0]  # 인덱스 뽑아서
+        chk_arr[index] = value  # 결과 배열 인덱스에 현재 배열 value 삽입
     stack.append((i, value))
 
 # for i in range(len(chk_arr)):
