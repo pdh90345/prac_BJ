74fd974f2d35eca4495451382372bba25c9390c4
pdh903 <pdh90345@gmail.com>
2024-08-12 15:32:48 +0900

    commit

---
+++ study_30/pg_예상대진표.py
@@ -0,0 +1,16 @@
+# 예상 대진표
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+
+def solution(n, a, b):
+    cnt = 1
+    while a != b:
+        cnt += 1
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+
+    return cnt
